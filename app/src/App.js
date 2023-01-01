9b69ead2b5f9a83d6f7b0fdcdfaf1bc0f325d616
Guilherme Barbosa <mrguilhermeferreira@gmail.com>
2023-01-01 18:46:50 -0300

    feat: adding musics data to ordering

--- app/src/App.js
+++ app/src/App.js
@@ -3,7 +3,7 @@ import { DragAndDropList } from "./components/DragAndDrop";
 function App() {
   return (
     <div className="flex flex-col items-center justify-center m-6">
-      <h1>Ordene as músicas de acordo com sua preferência</h1>
+      <h1 className="font-bold text-xl">Ordene as músicas de acordo com sua preferência</h1>
       <DragAndDropList />
     </div>
   );
--- app/src/components/DragAndDrop.jsx
+++ app/src/components/DragAndDrop.jsx
@@ -2,40 +2,46 @@ import { useState } from "react";
 import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
 
 export function DragAndDropList() {
-  const example_data = [
+  const data = [
     {
-      id: "gary",
-      name: "Gary Goodspeed",
-      thumb: "/images/gary.png",
+      id: "1",
+      name: "Calm Down",
+      thumb: "https://i.scdn.co/image/ab67616d0000b273a3a7f38ea2033aa501afd4cf",
+      artists: "Rema, Selena Gomez",
     },
     {
-      id: "cato",
-      name: "Little Cato",
-      thumb: "/images/cato.png",
+      id: "2",
+      name: "As It Was",
+      thumb: "https://i.scdn.co/image/ab67616d0000b273b46f74097655d7f353caab14",
+      artists: "Harry Styles",
     },
     {
-      id: "kvn",
-      name: "KVN",
-      thumb: "/images/kvn.png",
+      id: "3",
+      name: "In The End",
+      thumb: "https://i.scdn.co/image/ab67616d0000b273e2f039481babe23658fc719a",
+      artists: "Linkin Park"
     },
     {
-      id: "mooncake",
-      name: "Mooncake",
-      thumb: "/images/mooncake.png",
+      id: "4",
+      name: "Nothing Else Matters",
+      thumb: "https://i.scdn.co/image/ab67616d0000b27395958b5d219f1b328f083e08",
+      artists: "Metallica"
     },
     {
-      id: "quinn",
-      name: "Quinn Ergon",
-      thumb: "/images/quinn.png",
+      id: "5",
+      name: "Coração Radiante",
+      thumb: "https://i.scdn.co/image/ab67616d0000b2733a3569af7c0f73c3b7d74133",
+      artists: "Grupo Revelação"
     },
     {
-      id: "bacon",
-      name: "Bacon Bacon",
-      thumb: "/jaisadji/"
+      id: "6",
+      name: "Temporal",
+      thumb: "https://i.scdn.co/image/ab67616d0000b273fa79bbb1585d2580e484da5b",
+      artists: "Art Popular"
     }
   ];
 
-  const [musics, updateMusics] = useState(example_data);
+  const [musics, updateMusics] = useState(data);
 
   function handleOnDragEnd(result) {
     if (!result.destination) return;
@@ -56,7 +62,7 @@ export function DragAndDropList() {
             {...provided.droppableProps}
             ref={provided.innerRef}
           >
-            {musics.map(({id}, index) => {
+            {musics.map(({id, thumb, artists ,name}, index) => {
               return (
                 <Draggable key={id} draggableId={id} index={index}>
                   {(provided) => (
@@ -65,8 +71,12 @@ export function DragAndDropList() {
                       {...provided.draggableProps}
                       {...provided.dragHandleProps}
                     >
-                      <div className="flex items-center justify-center w-[12rem] h-64 bg-[#1b1b1b] rounded-md hover:brightness-125 transition-all ease-linear">
-                        Conteúdo da música
+                      <div className="flex flex-col items-center w-[12rem] h-64 bg-[#1b1b1b] p-3
+                      rounded-lg hover:brightness-125 transition-all ease-linear justify-between relative">
+                        <p className="absolute -left-2 -top-2 w-5 h-5 bg-[#1DB954] flex justify-center items-center rounded-full text-xs">{index+1}</p>
+                        <img src={thumb} alt="Description" className="w-full max-w-[160px] rounded-md shadow"/>
+                        <p className="text-center font-bold">{name}</p>
+                        <p className="opacity-70">{artists}</p>
                       </div>
                     </li>
                   )}
