549f3be1bf8862512aced59feec2f52812549ebb
Guilherme Barbosa Ferreira <mrguilhermeferreira@gmail.com>
2023-01-02 23:20:56 -0300

    fix: reordering pagode playlist data

--- app/src/utils/countingInversions.js
+++ app/src/utils/countingInversions.js
@@ -2,7 +2,7 @@ import { matchPlaylists } from "./matchPlaylists";
 
 const popPlaylist = ["2", "1", "6", "5", "3", "4"];
 const rockPlaylist = ["4", "3", "6", "5", "2", "1"];
-const pagodePlaylist = ["6", "5", "2", "1", "4", "3"];
+const pagodePlaylist = ["6", "5", "2", "3", "4", "1"];
 
 export function countingInversions(musicsOrdered) {
   let arrayData = [];
